a6211ec5d1d8a371ce4738175c1747dcacbc7fc7
RRJ <riyaranjanjha2006@gmail.com>
2024-05-21 19:54:28 +0530

    Changed Footer & Hover links

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import '../CSS/Footer.css';
 import { Facebook, Twitter, Instagram, LinkedIn} from '@mui/icons-material'; // Import the shoes icon
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const Footer = () => {
@@ -19,7 +20,7 @@ const Footer = () => {
                       type="email"
                       name="email"
                       placeholder="Enter Email"
-                      style={{ border: "3px solid var(--color-6)", borderRadius: "10px" }}
+                      style={{ border: "3px solid var(--color-6)", borderRadius: "10px", outline: "none"}}
                     />
             <button>Get monthly updates and free resources</button>
           </div>
@@ -33,9 +34,9 @@ const Footer = () => {
             <li onClick={()=>navigate('/contact')}>Get In Touch</li>
             <div className="social-icons">
               <Facebook />
-              <Twitter />
+              <Link to={"https://x.com/harshal_honde21"}><Twitter /></Link>
               <Instagram />
-              <LinkedIn />
+              <Link to={"https://www.linkedin.com/in/harshalhonde/"}><LinkedIn /></Link>
             </div>
           </ul>
         </div>
@@ -45,7 +46,7 @@ const Footer = () => {
           <z>Phone: +91 565 8989 878</z>
           <z>Email: spectastyle@gmail.com</z>
           <z>Address: 1234 Smart Street,Nagpur 440058</z>
-          <z style={{fontWeight:'bolder'}}>Design and Develop by <a style={{textDecoration:'none'}} href="https://harshalwebfolio.netlify.app">❤️Harshal Honde❤️</a></z>
+          <z style={{fontWeight:'bolder'}}>Designed and Developed by <a style={{textDecoration:'none'}} href="https://harshalwebfolio.netlify.app">❤️Harshal Honde❤️</a></z>
         </div>
 
         <div className="section">
